e4a7539d496959dbe2cef471540f7c9db98843ce
paolocastro <paolo.castro@gmail.com>
2018-06-16 22:34:58 +0200

    get the user data from facebook

--- src/FBLoginButton.js
+++ src/FBLoginButton.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { LoginButton } from 'react-native-fbsdk';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import InitUser  from '../user/InitUser';
 
 export default class FBLoginButton extends Component {
     render() {
         return (
             <View>
                 <LoginButton
-                    readPermissions={["public_profile email"]}
                     onLoginFinished={
                         (error, result) => {
-                            console.log('mlkmlk');
                             if (error) {
                                 alert("Login failed with error: " + error.message);
                             } else if (result.isCancelled) {
                                 alert("Login was cancelled");
                             } else {
-                                alert("Login was successful with permissions: " + result.grantedPermissions)
+                                AccessToken.getCurrentAccessToken().then((data) => {
+                                    console.log('mlk');
+                                    const { accessToken } = data;
+                                    console.log(accessToken);
+                                    var initUserObject = new InitUser();
+                                    initUserObject.init(accessToken);
+                                });
                             }
                         }
                     }
+                    readPermissions={["public_profile email"]}
                     onLogoutFinished={() => alert("User logged out")}/>
             </View>
         );
