568c726d69f2b13c6b9666da160e0d68ff1b9df8
paolocastro <paolo.castro@gmail.com>
2018-06-27 20:28:22 +0200

    add icons

--- android/app/src/main/java/com/union/MainApplication.java
+++ android/app/src/main/java/com/union/MainApplication.java
@@ -3,6 +3,7 @@ package com.union;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
           new RNFirebasePackage(),
           new FBSDKPackage(mCallbackManager),
           new RNFirebaseAnalyticsPackage(),
--- src/nav/Router.js
+++ src/nav/Router.js
@@ -3,12 +3,23 @@ import { createBottomTabNavigator, createStackNavigator } from 'react-navigation
 import List from "../screens/List";
 import UserProfile from "../screens/UserProfile";
 import Login from "../screens/Login";
+import Icon from  "react-native-vector-icons/FontAwesome";
 
 
 export const SignedIn = createBottomTabNavigator(
     {
-        TimeLine: {screen: List},
-        Profile: {screen: UserProfile}
+        TimeLine: {
+            screen: List,
+            navigationOptions: {
+                tabBarLabel: 'Feed',
+                tabBarIcon: ({ tintColor }) => <Icon name="list" size={30} color={tintColor} />
+            }},
+        Profile: {
+            screen: UserProfile,
+            navigationOptions: {
+                tabBarLabel: 'Me',
+                tabBarIcon: ({ tintColor }) => <Icon name="user-circle" size={30} color={tintColor} />
+            },}
     },
     {
         tabBarPosition: 'bottom',
